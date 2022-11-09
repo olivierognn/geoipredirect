dcc3c81d8b3b8b28a832b7dca7d9cac185d44b1f
Gennaro Oliviero <gennaro.oliviero@bitbull.it>
2022-11-09 18:25:18 +0100

    initial commit

---
+++ app/code/local/Amasty/GeoipRedirect/Model/System/Config/Backend/Country.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* @author Amasty Team
+* @copyright Copyright (c) 2022 Amasty (https://www.amasty.com)
+* @package GeoIP Redirect
+*/
+class Amasty_GeoipRedirect_Model_System_Config_Backend_Country extends Mage_Adminhtml_Model_System_Config_Backend_Serialized_Array
+{
+    protected $_eventPrefix = 'core_config_backend_country';
+}
---
+++ app/code/local/Amasty/GeoipRedirect/sql/amgeoipredirect_setup/upgrade-1.0.12-1.0.13.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* @author Amasty Team
+* @copyright Copyright (c) 2022 Amasty (https://www.amasty.com)
+* @package GeoIP Redirect
+*/
+/**
+ *
+ * Adding the default data to the config amgeoipredirect/restriction/excepted_urls
+ */
+
+$this->startSetup();
+$path = 'amgeoipredirect/restriction/excepted_urls';
+$nameTable = $this->getTable('core/config_data');
+$defaultUrls = PHP_EOL . Mage::getConfig()->getNode('default/' . $path);
+$defaultUrls = addslashes($defaultUrls);
+$this->run("UPDATE `{$nameTable}` SET `value` = CONCAT(`value`,'{$defaultUrls}') WHERE `path` = '{$path}'");
+$this->endSetup();
